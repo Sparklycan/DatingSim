d7d605a90acf05d76d67522e31066ef2d50daa07
fuiuki <fuiuki.linorva@gmail.com>
2022-02-08 09:41:50 +0100

    Layers and one script added

---
+++ git-sprojectproject/Assets/Rebecka/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+   //float firerate
+   //private float timer
+   //private floats for love, lust, sus
+   
+    
+    
+   //update: movement och fire (vilken knapp) 
+   
+   //on trigger enter
+   //projectiles: damage
+   //points: value1 or value2
+   
+   
+    //void fire
+}
