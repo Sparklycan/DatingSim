893fd230cd78e9af9ca21d0b67373fb8b35da226
fuiuki <fuiuki.linorva@gmail.com>
2022-03-02 14:59:01 +0100

    Bird game stuff

--- git-sprojectproject/Assets/Rebecka/Scripts/BirdS/PlayerScript.cs
+++ git-sprojectproject/Assets/Rebecka/Scripts/BirdS/PlayerScript.cs
@@ -6,12 +6,19 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public GameObject projectile;
+
+    public float speed;
+    public float jumpForce;
+    private Rigidbody2D rb;
    //float firerate
    //private float timer
    //private floats for love, lust, sus
-   
-    
-    
+
+   private void Start()
+   {
+       rb = GetComponent<Rigidbody2D>();
+   }
+
    //update: movement och fire (vilken knapp) 
    private void Update()
    {
@@ -22,6 +29,20 @@ public class PlayerScript : MonoBehaviour
            Instantiate(projectile, transform.position, Quaternion.identity);
 
        }
+
+       if (Input.GetKeyDown(KeyCode.Space))
+       {
+           rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+       }
+
+       Move();
+   }
+
+   private void Move()
+   {
+       float x = Input.GetAxisRaw("Horizontal"); 
+       float step = x * speed; 
+       rb.velocity = new Vector2(step, rb.velocity.y); 
    }
 
    //on trigger enter
