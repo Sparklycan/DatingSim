8695d7b5768f21c2c923b5cc7482888c513b8f12
Sparklycan <hannes.palm@hotmail.se>
2022-03-02 15:10:48 +0100

    Lighting, navmesh and fishpong fixat.

--- git-sprojectproject/Assets/hannes/Scripts/Stealth/PoliceScript.cs
+++ git-sprojectproject/Assets/hannes/Scripts/Stealth/PoliceScript.cs
@@ -28,6 +28,8 @@ public class PoliceScript : MonoBehaviour
     private GameObject player;
 
     private Light _light;
+
+    public bool Killable;
     // Attention area
     [Header("ViewArea")]
     public float distance = 10;
@@ -130,6 +132,7 @@ public class PoliceScript : MonoBehaviour
     private BoxCollider boxCollider;
     void Start()
     {
+
         Friends.Clear();
         GameObject[] AIArray = GameObject.FindGameObjectsWithTag("AI");
         foreach (GameObject node in AIArray)
@@ -142,6 +145,10 @@ public class PoliceScript : MonoBehaviour
         path = new NavMeshPath();
         _light = GetComponent<Light>();
         boxCollider = GetComponent<BoxCollider>();
+        if (Killable)
+        {
+            boxCollider.enabled = true;
+        }
         slider.gameObject.SetActive(false);
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("StealthPlayer");
@@ -161,7 +168,7 @@ public class PoliceScript : MonoBehaviour
             }
         }
         SpotLight.spotAngle = angle * 2;
-        SpotLight.range = distance + 2;
+        SpotLight.range = distance * 2;
         if (Input.GetKeyDown(KeyCode.C))
         {
             agent.SetDestination(player.transform.position);
