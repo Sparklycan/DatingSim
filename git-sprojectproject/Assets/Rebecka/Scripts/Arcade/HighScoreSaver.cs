28755bab1ff0468d1d4e20d48eb3a124461246e4
fuiuki <fuiuki.linorva@gmail.com>
2022-03-16 10:20:41 +0100

    Beep

--- git-sprojectproject/Assets/Rebecka/Scripts/Arcade/HighScoreSaver.cs
+++ git-sprojectproject/Assets/Rebecka/Scripts/Arcade/HighScoreSaver.cs
@@ -34,9 +34,21 @@ public class HighScoreSaver : MonoBehaviour
         data.values3ay = scoreContainer.fishpongScore3.y;
         data.values3az = scoreContainer.fishpongScore3.z;
         
-        data.values1bx = scoreContainer.platformerTimeScores.x;
-        data.values1by = scoreContainer.platformerTimeScores.y;
-        data.values1bz = scoreContainer.platformerTimeScores.z;
+        data.values1bx = scoreContainer.platformerTimeScoresFun.x;
+        data.values1by = scoreContainer.platformerTimeScoresFun.y;
+        data.values1bz = scoreContainer.platformerTimeScoresFun.z;
+        
+        data.values2bx = scoreContainer.platformerTimeScoresGood.x;
+        data.values2by = scoreContainer.platformerTimeScoresGood.y;
+        data.values2bz = scoreContainer.platformerTimeScoresGood.z; 
+        
+        data.values3bx = scoreContainer.platformerKillsScores.x;
+        data.values3by = scoreContainer.platformerKillsScores.y;
+        data.values3bz = scoreContainer.platformerKillsScores.z;
+
+        data.values1dx = scoreContainer.flexTurnScores.x;
+        data.values1dy = scoreContainer.flexTurnScores.y;
+        data.values1dz = scoreContainer.flexTurnScores.z;
         
         data.values1ex = scoreContainer.sweeperScore1.x;
         data.values1ey = scoreContainer.sweeperScore1.y;
@@ -82,9 +94,21 @@ public class HighScoreSaver : MonoBehaviour
                 scoreContainer.fishpongScore3.y = data.values3ay;
                 scoreContainer.fishpongScore3.z = data.values3az;
                 
-                scoreContainer.platformerTimeScores.x = data.values1bx;
-                scoreContainer.platformerTimeScores.y = data.values1by;
-                scoreContainer.platformerTimeScores.z = data.values1bz;
+                scoreContainer.platformerTimeScoresFun.x = data.values1bx;
+                scoreContainer.platformerTimeScoresFun.y = data.values1by;
+                scoreContainer.platformerTimeScoresFun.z = data.values1bz;
+                
+                scoreContainer.platformerTimeScoresGood.x = data.values2bx;
+                scoreContainer.platformerTimeScoresGood.y = data.values2by;
+                scoreContainer.platformerTimeScoresGood.z = data.values2bz;
+                
+                scoreContainer.platformerKillsScores.x = data.values3bx;
+                scoreContainer.platformerKillsScores.y = data.values3by;
+                scoreContainer.platformerKillsScores.z = data.values3bz;
+                
+                scoreContainer.flexTurnScores.x = data.values1dx;
+                scoreContainer.flexTurnScores.y = data.values1dy;
+                scoreContainer.flexTurnScores.z = data.values1dz;
                 
                 scoreContainer.sweeperScore1.x = data.values1ex;
                 scoreContainer.sweeperScore1.y = data.values1ey;
@@ -105,59 +129,12 @@ public class HighScoreSaver : MonoBehaviour
                 Debug.Log("ur a big dummy, no save here!");
             }
         //}
-        /*else
-        {
-            if (File.Exists( customPath + "/MyHighscoreSaveData.dat"))
-            {
-                BinaryFormatter bf = new BinaryFormatter();
-                FileStream file = 
-                    File.Open(customPath + "/MyHighscoreSaveData.dat", FileMode.Open);
-                HighscoreSaveData data = (HighscoreSaveData)bf.Deserialize(file);
-                file.Close();
-                scoreContainer.useDefaultPath = data.defaultPathUsed;
-                customPath = data.customPath;
-                scoreContainer.useDefaultPath = data.defaultPathUsed;
-                customPath= data.customPath;
-                scoreContainer.fishpongScore1.x = data.values1ax;
-                scoreContainer.fishpongScore1.y = data.values1ay;
-                scoreContainer.fishpongScore1.z = data.values1az;
-                
-                scoreContainer.fishpongScore2.x = data.values2ax;
-                scoreContainer.fishpongScore2.y = data.values2ay;
-                scoreContainer.fishpongScore2.z = data.values2az;
-                
-                scoreContainer.fishpongScore3.x = data.values3ax;
-                scoreContainer.fishpongScore3.y = data.values3ay;
-                scoreContainer.fishpongScore3.z = data.values3az;
-                
-                scoreContainer.platformerTimeScores.x = data.values1bx;
-                scoreContainer.platformerTimeScores.y = data.values1by;
-                scoreContainer.platformerTimeScores.z = data.values1bz;
-                
-                scoreContainer.sweeperScore1.x = data.values1ex;
-                scoreContainer.sweeperScore1.y = data.values1ey;
-                scoreContainer.sweeperScore1.z = data.values1ez;
-                
-                scoreContainer.sweeperScore2.x = data.values2ex;
-                scoreContainer.sweeperScore2.y = data.values2ey;
-                scoreContainer.sweeperScore2.z = data.values2ez;
-                
-                scoreContainer.sweeperScore3.x = data.values3ex;
-                scoreContainer.sweeperScore3.y = data.values3ey;
-                scoreContainer.sweeperScore3.z = data.values3ez;
-                Debug.Log("Game data loaded!");
-            }
-            else
-            {
-                Debug.Log("ur a big dummy, no save here!");
-            }
-        }*/
+
     }
 
     public void ResetData()
     {
-        //if (scoreContainer.useDefaultPath)
-       // {
+       
             if (File.Exists( Application.persistentDataPath 
                              +"/MyHighscoreSaveData.dat"))
             {
@@ -166,7 +143,10 @@ public class HighScoreSaver : MonoBehaviour
                 scoreContainer.fishpongScore1 = Vector3.zero;
                 scoreContainer.fishpongScore2 = Vector3.zero;
                 scoreContainer.fishpongScore3 = Vector3.zero;
-                scoreContainer.platformerTimeScores = Vector3.zero;
+                scoreContainer.platformerTimeScoresFun = Vector3.zero;
+                scoreContainer.platformerTimeScoresGood = Vector3.zero;
+                scoreContainer.platformerKillsScores = Vector3.zero;
+                scoreContainer.flexTurnScores = Vector3.zero;
                 scoreContainer.sweeperScore1 = Vector3.zero;
                 scoreContainer.sweeperScore2 = Vector3.zero;
                 scoreContainer.sweeperScore3 = Vector3.zero;
@@ -176,28 +156,8 @@ public class HighScoreSaver : MonoBehaviour
             {
                 Debug.Log("bro there's nothing HERE");
             }
-      //  }
-        /*else
-        {
-            if (File.Exists(customPath + "/MyHighscoreSaveData.dat"))
-            {
-                File.Delete(customPath + "/MyHighscoreSaveData.dat");
-                scoreContainer.useDefaultPath = true;
-                customPath = "C:/Users/YourUser/AppData/LocalLow";
-                scoreContainer.fishpongScore1 = Vector3.zero;
-                scoreContainer.fishpongScore2 = Vector3.zero;
-                scoreContainer.fishpongScore3 = Vector3.zero;
-                scoreContainer.platformerTimeScores = Vector3.zero;
-                scoreContainer.sweeperScore1 = Vector3.zero;
-                scoreContainer.sweeperScore2 = Vector3.zero;
-                scoreContainer.sweeperScore3 = Vector3.zero;
-                Debug.Log("Data reset done, yeet");
-            }
-            else
-            {
-                Debug.Log("bro there's nothing HERE");
-            }
-        }*/
+
+ 
        
     }
     
@@ -229,10 +189,22 @@ class HighscoreSaveData
     public float values1bx;
     public float values1by;
     public float values1bz;
+    
+    public float values2bx;
+    public float values2by;
+    public float values2bz;
+    
+    public float values3bx;
+    public float values3by;
+    public float values3bz;
 
     //stealth
     
     //Flex game
+    
+    public float values1dx;
+    public float values1dy;
+    public float values1dz;
 
     //sweeper
     public float values1ex;
--- git-sprojectproject/Assets/Rebecka/Scripts/Arcade/HighscoreContainer.cs
+++ git-sprojectproject/Assets/Rebecka/Scripts/Arcade/HighscoreContainer.cs
@@ -9,7 +9,7 @@ public class HighscoreContainer : ScriptableObject
 {
     //x = love, y = lust, z = sus
     [HideInInspector]public Vector3 fishpongScore1 = new Vector3(0,0,0 ), fishpongScore2 = new Vector3(0,0,0 ), fishpongScore3 =new Vector3(0,0,0 );
-    [HideInInspector]public Vector3 platformerTimeScores = new Vector3(0,0,0 );
+    [HideInInspector]public Vector3 platformerTimeScoresFun = new Vector3(0,0,0 ), platformerTimeScoresGood = new Vector3(0,0,0 ), platformerKillsScores = new Vector3(0,0,0 );
     [HideInInspector]public Vector3 sweeperScore1 = new Vector3(0,0,0), sweeperScore2 = new Vector3(0,0,0), sweeperScore3 = new Vector3(0,0,0);
     [HideInInspector]public Vector3 stealthSuspicion = new Vector3(0, 0, 0), stealthNuggetScores = new Vector3(0,0,0);
     [HideInInspector]public Vector3 flexTurnScores = new Vector3(0,0,0);
