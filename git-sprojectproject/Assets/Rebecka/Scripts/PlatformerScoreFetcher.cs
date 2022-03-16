cf0dc55576a0f06814dde9cbf2b488f161201907
fuiuki <fuiuki.linorva@gmail.com>
2022-03-16 11:42:40 +0100

    Score progress arcade

---
+++ git-sprojectproject/Assets/Rebecka/Scripts/PlatformerScoreFetcher.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerScoreFetcher : MonoBehaviour
+{
+    public bool includeKills = false;
+    public Controller playerScript;
+   public MinigameHighscoreManager scoreManager;
+   public int timerValue, kills;
+
+}
